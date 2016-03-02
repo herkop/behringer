92af0417c27805fba9fd3902472bbb0632eed742
Herko Pirk <herko.pirk@gmail.com>
2016-03-02 12:52:31 +0200

    database

--- index.php
+++ index.php
@@ -7,3 +7,12 @@
  */
 
 echo"Hello world!";
+
+$url = parse_url(getenv("postgres://kmeuaardudjwqp:qUyoFbXt78mblu_CEcezXhgCxw@ec2-54-227-250-148.compute-1.amazonaws.com:5432/d7fdmpdh0nvpmu"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
