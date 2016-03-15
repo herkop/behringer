5a8a3bb2e4f513cadf79c0031916104570950671
Herko Pirk <herko.pirk@gmail.com>
2016-03-15 10:46:37 +0200

    Database sql tables

--- prototype/sCfhT/gup.php
+++ prototype/sCfhT/gup.php
@@ -24,14 +24,13 @@ if($_POST["run"]){
         $err = "Failed!";
     }
 
-        $error = pg_result_error($result);
-
+        $error = pg_last_error($result);
 }
-
+pg_close($db);
 ?>
 
 <form action="" method="post" name="data">
-    <label>Tekst siia1:</label>
+    <label>Tekst siia:</label>
 <textarea name="sentence"><?php echo $comment?></textarea>
 <input value="Run" type="submit" name="run">
     <div ><br>Query: <?php echo $comment;?><br>
