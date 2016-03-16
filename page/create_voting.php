e6b74acf5924a3406fddefba1d7994c716eb9489
Herko Pirk <herko.pirk@gmail.com>
2016-03-16 11:28:27 +0200

    voting

---
+++ page/create_voting.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<form action="" method="post", name="create_voting">
+    <b>Pealkiri: </b>
+    <input type="text" name="title">
+    <b>Algus aeg:</b>
+    <input type="datetime" name="start_datetime">
+    <b>Lõpu aeg:</b>
+    <input type="datetime" name="finish_datetime">
+    <input type="submit" name="new_voting">
+</form>
