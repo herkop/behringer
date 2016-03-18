e46597a4c38fca882cfec5c9faaaaf7c6eae8d78
Herko Pirk <herko.pirk@gmail.com>
2016-03-18 12:11:00 +0200

    candidate

--- page/candidate.php
+++ page/candidate.php
@@ -5,32 +5,7 @@
         <td><b>Erakond</b></td>
         <td><b>Number</b></td>
     </tr>
-    <?php
-    require "../data/config.php";
 
-    if($db) {
-        $er = "tere";
-        $result = pg_query($db, "SELECT firstname, lastname, voting, votenumber, party FROM candidate");
-        while($row = pg_fetch_assoc($result)){
-            $firstname = $row["firstname"];
-            $lastname = $row["lastname"];
-            $votenumber = $row["votenumber"];
-            $party = $row["party"];
-
-            echo"<tr>
-                <td>$firstname $lastname</td>
-                <td>$party</td>
-                <td>$votenumber</td>
-                </tr>";
-        }
-        $er = pg_last_error($db);
-        pg_close($db);
-    }
-    else{
-        $er = "fail";
-    }
-    $er = "ffffffffffffffff".pg_last_error($db);
-    ?>
     <tr>
         <td>Mari Mets</td>
         <td>Kollased</td>
