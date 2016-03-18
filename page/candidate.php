33d658fb54eee70bb9a50be2d0406851902c5cab
Herko Pirk <herko.pirk@gmail.com>
2016-03-18 12:30:59 +0200

    candidate

--- page/candidate.php
+++ page/candidate.php
@@ -1,29 +1,65 @@
 
-<table>
-    <tr>
-        <td><b>Nimi</b></td>
-        <td><b>Erakond</b></td>
-        <td><b>Number</b></td>
-    </tr>
+<b>Valimised:</b>
+<form method="post" action="" name="show_voting">
+<select name="voting">
+    <option value="0" <?php if(!$voting)echo"selected='selected'"?> disabled="disabled">Vali</option>
     <?php
-    include "../data/config.php";
-
-    if($db) {
-        $result = pg_query($db, "SELECT firstname, lastname, voting, votenumber, party FROM candidate");
+    if($db){
+        $result = pg_query($db, "SELECT * FROM voting");
         while($row = pg_fetch_assoc($result)){
-            $firstname = $row["firstname"];
-            $lastname = $row["lastname"];
-            $votenumber = $row["votenumber"];
-            $party = $row["party"];
+            $id = $row["id"];
+            $title = $row["title"];
+            if($voting == $id) {
+                echo "<option value='$id' selected='selected'>$title</option>";
+            }
+            else{
+                echo "<option value='$id'>$title</option>";
+            }
+        }
+        pg_close($db);
+    }
 
-            echo"<tr>
+    ?>
+
+</select>
+<input type="submit" name="select_voting">
+    </form>
+<?php
+    if($_POST["select_voting"]) {
+        $voting = $_POST["voting"];
+        if($voting) {
+
+
+            ?>
+            <table>
+                <tr>
+                    <td><b>Nimi</b></td>
+                    <td><b>Erakond</b></td>
+                    <td><b>Number</b></td>
+                </tr>
+                <?php
+                include "../data/config.php";
+
+                if ($db) {
+                    $result = pg_query($db, "SELECT firstname, lastname, voting, votenumber, party FROM candidate WHERE voting = '" .$voting. "'");
+                    while ($row = pg_fetch_assoc($result)) {
+                        $firstname = $row["firstname"];
+                        $lastname = $row["lastname"];
+                        $votenumber = $row["votenumber"];
+                        $party = $row["party"];
+
+                        echo "<tr>
                 <td>$firstname $lastname</td>
                 <td>$party</td>
                 <td>$votenumber</td>
                 </tr>";
-        }
+                    }
 
-        pg_close($db);
+                    pg_close($db);
+                }
+                ?>
+            </table>
+            <?php
+        }
     }
-    ?>
-</table>
+?>
