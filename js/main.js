6fdfd3f7d557113c15919cef0c724ead46e98c13
Herko Pirk <herko.pirk@gmail.com>
2016-03-19 22:58:29 +0200

    candidate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
     var today = new Date().toISOString().split('T')[0];
     //document.getElementsByName("start_date")[0].setAttribute('min', today);
-    document.create_voting.start_date.setAttribute('min', today);
+    document.create_voting.start_date[0].setAttribute('min', today);
     document.getElementsByName("finish_date")[0].setAttribute('min', today);
     document.getElementsByName("start_date")[0].addEventListener('change', changeDateSelect);
 
