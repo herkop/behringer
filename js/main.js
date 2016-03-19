96d2ea52a51f949c8472e9096eb126348d117689
Herko Pirk <herko.pirk@gmail.com>
2016-03-19 23:20:06 +0200

    candidate

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-    window.onload = function(){
-        var today = new Date().toISOString().split('T')[0];
-    //document.getElementsByName("start_date")[0].setAttribute('min', today);
-    document.getElementById("voting_date").setAttribute('min', today);
-    document.getElementsByName("finish_date")[0].setAttribute('min', today);
-    document.getElementsByName("start_date")[0].addEventListener('change', changeDateSelect);
+    window.onload = function() {
+        if (document.getElementsByName("start_date")[0] && document.getElementsByName("start_date")[0]) {
 
+        var today = new Date().toISOString().split('T')[0];
+        //document.getElementsByName("start_date")[0].setAttribute('min', today);
+        document.getElementById("voting_date").setAttribute('min', today);
+        document.getElementsByName("finish_date")[0].setAttribute('min', today);
+        document.getElementsByName("start_date")[0].addEventListener('change', changeDateSelect);
+    }
 
     function changeDateSelect() {
         var second = document.getElementsByName("start_date")[0].value;
