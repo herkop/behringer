b2c33317959970c1871f9408a72adbd1e1d6d43a
Herko Pirk <herko.pirk@gmail.com>
2016-03-19 23:18:06 +0200

    candidate

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-    var today = new Date().toISOString().split('T')[0];
+    window.onload = function(){
+        var today = new Date().toISOString().split('T')[0];
     //document.getElementsByName("start_date")[0].setAttribute('min', today);
     document.getElementById("voting_date").setAttribute('min', today);
     document.getElementsByName("finish_date")[0].setAttribute('min', today);
     document.getElementsByName("start_date")[0].addEventListener('change', changeDateSelect);
 
 
-function changeDateSelect(){
-    var second = document.getElementsByName("start_date")[0].value;
-    document.getElementsByName("finish_date")[0].setAttribute('min', second);
-}
+    function changeDateSelect() {
+        var second = document.getElementsByName("start_date")[0].value;
+        document.getElementsByName("finish_date")[0].setAttribute('min', second);
+    }
+
+    };
