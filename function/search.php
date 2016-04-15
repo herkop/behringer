234349f754ffc1bbc1d0190b726aa415f0aaa14d
Herko <herko.pirk@gmail.com>
2016-04-15 15:10:20 +0300

    search

--- function/search.php
+++ function/search.php
@@ -4,7 +4,7 @@ $key = $_GET['key'];
 $array = array();
 include "../data/config.php";
 
-$result1 = pg_query($db, "SELECT firstname, lastname FROM candidate WHERE firstname LIKE '%m%'");
+$result1 = pg_query($db, "SELECT firstname, lastname FROM candidate WHERE firstname LIKE 'm%'");
 while ($row = pg_fetch_assoc($result1)) {
     $firstname = $row["firstname"];
     $lastname = $row["lastname"];
