37c585d0e181dd6536c712d8eefa30b88f4aa9d8
Herko <herko.pirk@gmail.com>
2016-04-15 16:54:06 +0300

    map

--- page/contact.php
+++ page/contact.php
@@ -1,4 +1,4 @@
-<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script src="https://maps.googleapis.com/maps/api/js"></script>
 <script>
     google.maps.event.addDomListener(window, 'load', initMap);
     function initMap() {
