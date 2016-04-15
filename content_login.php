f06ea19cfe12c265885a0e8d87e9ec8a070bbbca
Herko <herko.pirk@gmail.com>
2016-04-15 14:57:25 +0300

    search

--- content_login.php
+++ content_login.php
@@ -104,7 +104,7 @@ if(!isset($logged_user)) {
 		<a href="#" onclick="signOut();">Sign out</a>
 		<script>
 			function signOut() {
-				var auth2 = googleUser.getAuthInstance();
+				var auth2 = gapi.auth2.getAuthInstance();
 				auth2.signOut().then(function () {
 					console.log('User signed out.');
 				});
---
+++ function/search.php
@@ -0,0 +1,14 @@
+<?php
+
+$key = $_GET['key'];
+$array = array();
+include "../data/config.php";
+
+$result1 = pg_query($db, "SELECT firstname, lastname FROM candidate WHERE firstname LIKE '%$key%'");
+while ($row = pg_fetch_assoc($result1)) {
+    $firstname = $row["firstname"];
+    $lastname = $row["lastname"];
+    $array[] = $firstname . " " . $lastname;
+}
+
+echo json_decode($array);
