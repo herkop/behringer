659884bdd59d6cf7639e28731e0ba51005c3d48e
Herko <herko.pirk@gmail.com>
2016-04-24 16:29:00 +0300

    search test

--- header.php
+++ header.php
@@ -8,4 +8,5 @@
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="/js/main.js"></script>
+    <script src="/js/typeahead.bundle.js"></script>
 </head>
