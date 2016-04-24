dacbcf5e81e8d36be12da4ff4ab039f510f4e93a
Herko <herko.pirk@gmail.com>
2016-04-24 20:48:46 +0300

    load data when scroll

--- function/load_more.php
+++ function/load_more.php
@@ -3,6 +3,8 @@ include "../data/config.php";
 $last_cand = $_GET['last_cand'];
 $out = explode("CD", $last_cand)[1];
 $vote = explode("-", $out);
+echo $vote[0];
+echo $vote[1];
 $result = pg_query($db, "SELECT * FROM candidate WHERE voting = $vote[0] AND id < '$vote[1]' ORDER BY id DESC LIMIT 5");
 while ($row = pg_fetch_assoc($result)){
     $id = $row['id'];
