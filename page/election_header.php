361eab2453109de7b5d536d7c67da60931309fd0
Herko <herko.pirk@gmail.com>
2016-04-24 23:08:52 +0300

    ajax

---
+++ page/election_header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Valimistest</title>
+	</head>
+	<body>
+		<nav id="main">
+			<a href="?page=election_kov">Kohalikud valimised</a>
+			<a href="?page=election_pres">Presidendi valimised</a>
+			<a href="?page=election_rk">Riigikogu valimised</a>
+		</nav>
